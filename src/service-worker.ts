9bd1562e9b7ca019070673809ca9017ace666d70
Emmanuel Campuzano <40412180+ShiWonWan@users.noreply.github.com>
2022-11-19 14:24:34 -0600

    service worker definition

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,12 +1,6 @@
 /// <reference lib="webworker" />
 /* eslint-disable no-restricted-globals */
 
-// This service worker can be customized!
-// See https://developers.google.com/web/tools/workbox/modules
-// for the list of available Workbox modules, or add any other
-// code you'd like.
-// You can also remove this file if you'd prefer not to use a
-// service worker, and the Workbox build step will be skipped.
 
 import { clientsClaim } from 'workbox-core';
 import { ExpirationPlugin } from 'workbox-expiration';
@@ -76,5 +70,3 @@ self.addEventListener('message', (event) => {
     self.skipWaiting();
   }
 });
-
-// Any other custom service worker logic can go here.
